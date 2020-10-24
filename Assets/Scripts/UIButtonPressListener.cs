712d7947a08dce123aae7310b494c1b637613915
request <iamrequest@protonmail.com>
2020-10-23 23:44:38 -0400

    More poking at canvas UI, no good/easy sol yet

---
+++ Assets/Scripts/UIButtonPressListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+        
+/// <summary>
+/// Listens for simple trigger enters, and invokes the button.onClick event
+/// </summary>
+public class UIButtonPressListener : MonoBehaviour {
+    private Button button;
+    public string tag;
+
+    private void Awake() {
+        button = GetComponent<Button>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // Make sure it's the player interacting with the trigger
+        if (other.CompareTag("Player")) {
+            button.onClick.Invoke();
+        }
+    }
+}
