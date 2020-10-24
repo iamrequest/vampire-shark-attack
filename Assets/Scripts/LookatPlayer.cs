51af6c3523bc64a5feba8628a3ac4627c7d77ab5
request <iamrequest@protonmail.com>
2020-10-23 22:54:55 -0400

    Added world dialog
    - Added Fingerpaint Regular font
    	- Configured to work with Textmesh pro
    - Added dialog prefab
    	- Looks at player, with option to constrain rotation to Y axis

---
+++ Assets/Scripts/LookatPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class LookatPlayer : MonoBehaviour {
+    public bool constraintRotationToYAxis;
+
+    private void Update() {
+        if (constraintRotationToYAxis) {
+            transform.forward = Vector3.ProjectOnPlane(transform.position - Player.instance.hmdTransform.position, Vector3.up).normalized;
+        } else {
+            transform.LookAt(Player.instance.hmdTransform.position);
+        }
+    }
+}
