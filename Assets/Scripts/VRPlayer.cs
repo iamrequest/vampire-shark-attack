2b9e21846e9568d2ba48df364572ba8757414cc8
request <iamrequest@protonmail.com>
2020-10-24 22:51:35 -0400

    Began work on player death
    - Added waypoint functionality
    - Added ability to kill player
    - Tweaked character controller size, for better feel
    - Made snap turning faster

---
+++ Assets/Scripts/VRPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRPlayer : MonoBehaviour {
+    // Singleton pattern
+    private static VRPlayer _instance;
+    public static VRPlayer instance {
+        get {
+            return _instance;
+        }
+    }
+
+    public Waypoint currentWaypoint;
+    private CharacterController characterController;
+
+    private void Awake() {
+        if (_instance != null) {
+            Debug.LogError("There can only be one instance of VRPlayer!");
+            Destroy(this);
+        }
+        _instance = this;
+    }
+
+    private void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void SendToLastWaypoint(float delay) {
+        StartCoroutine(DoSendToLastWaypoint(delay));
+    }
+
+    private IEnumerator DoSendToLastWaypoint(float delay) {
+        yield return new WaitForSeconds(delay);
+
+        // Prevent the CC from overwriting our movement
+        characterController.enabled = false;
+
+        transform.position = currentWaypoint.transform.position;
+        transform.rotation = currentWaypoint.transform.rotation;
+
+        characterController.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class Waypoint : MonoBehaviour {
+    [Tooltip("")]
+    public int priority;
+    public UnityEvent onWaypointSet;
+
+    private void OnTriggerEnter(Collider other) {
+        // Confirm that whatever entered the waypoint's trigger space is the player
+        if (other.gameObject.CompareTag("Player")) {
+            // If the player doesn't already have a waypoint, then set it.
+            if (VRPlayer.instance.currentWaypoint == null) {
+                VRPlayer.instance.currentWaypoint = this;
+                onWaypointSet.Invoke();
+            } else {
+                // If the existing waypoint has lower priority than this one, then overwrite it
+                if (VRPlayer.instance.currentWaypoint.priority < priority) {
+                    onWaypointSet.Invoke();
+                }
+            }
+        }
+    }
+}
