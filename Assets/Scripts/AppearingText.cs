053114dc7f0becb4fa4971a9af0ecb17af252211
request <iamrequest@protonmail.com>
2020-10-25 00:30:23 -0400

    Began working on main level, with proper props
    - Added appearing dialog prefab (alpha lerps from 0->1 on trigger)
    - Replaced whitebox meshes in main level with real objects (ie: same thing but with colliders)

---
+++ Assets/Scripts/AppearingText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// On trigger, play an animation.
+/// </summary>
+[RequireComponent(typeof(Collider))]
+public class AppearingText : MonoBehaviour {
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            animator.SetTrigger("ShowText");
+        }
+    }
+}
