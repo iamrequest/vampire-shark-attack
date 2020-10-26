a52d3c8ab803e13fb33d64518cd46a6e4263ac8a
request <iamrequest@protonmail.com>
2020-10-26 15:10:10 -0400

    Began work on final cinematic
    - Added lever to trigger animation
    - Added gate animation
    
    Final steps: Add shark animation

---
+++ Assets/Scripts/OnTriggerEventEmitter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEventEmitter : MonoBehaviour {
+    public GameObject target;
+    public UnityEvent onTriggerEvent;
+
+    private void OnTriggerEnter(Collider other) {
+        // Quick way to confirm the object is what we expect
+        // Better way would be to compare tag, or layer
+        if (other.gameObject == target) {
+            onTriggerEvent.Invoke();
+        }
+    }
+}
